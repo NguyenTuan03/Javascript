a453446405c20ee3f749082ebe45529ea37710fa
Tuan Nguyen <tuannase170164@fpt.edu.vn>
2023-07-06 20:34:40 +0700

    square

---
+++ Day 4 - variable/Square.js
@@ -0,0 +1,12 @@
+let width = prompt("Nhap width");
+let height = prompt("Nhap heigh");
+let s = width * height;
+document.write("Enter width:");
+document.write("<br>");
+document.write(width);
+document.write("<br>");
+document.write("Enter height:");
+document.write("<br>");
+document.write(height);
+document.write("<br>")
+document.write("Square is: ",s);
